e8e711591e05cf736cfd6d3277ed6ee56fbb2035
unknown <godstimeonyibe2@gmail.com>
2020-09-09 14:23:45 +0100

    implemented the weather app by adding functionality to it

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,31 @@
-let date = new Date()
-let cYear = date.getFullYear()
+// ===== The date that is place at the footer =====
+const date = new Date()
+const cYear = date.getFullYear()
 document.querySelector('.year').innerHTML = cYear
+
+//  the api key
+const api = {
+  key: 'faf89cd0be99a684d9fe16932eb2d34b',
+  baseUrl: 'http://api.openweathermap.org/data/2.5/'
+}
+
+const searchBtn = document.querySelector('.btn').addEventListener('click', function () {
+  const search = document.querySelector('#inputSearch')
+  getRecord(search.value)
+})
+
+const getRecord = (qry) => {
+  fetch(`${api.baseUrl}weather?q=${qry}&APPID=${api.key}&units=metric`)
+    .then(weather => {
+      return weather.json()
+    }).then(displayRecord)
+}
+
+const displayRecord = (weather) => {
+  console.log(weather)
+  const city = document.querySelector('.city').innerHTML = `${weather.name}`
+  const country = document.querySelector('.country').innerHTML = `${weather.sys.country}`
+  const temp = document.querySelector('.temp').innerHTML = `${Math.round(weather.main.temp)}<span>&#176;C</span>`
+  const weatherDescription = document.querySelector('.weatherEl').innerHTML = weather.weather[0].main
+  // const iconEL = document.querySelector('.date').innerHTML = weather.weather[1].description
+}
